3c926266071c24361569b49b7353ce97087fb349
Miguel <fernandezmonzonmiguel@gmail.com>
2026-05-29 22:24:41 -0300

    Dominio (Usuario): corrección minima en Rol.

--- Dominio/Dominio/Usuario.cs
+++ Dominio/Dominio/Usuario.cs
@@ -9,10 +9,9 @@ namespace Dominio
     public class Usuario
     {
         public int IdUsuario { get; set; }
-        public int IdRol { get; set; }
+        string Rol { get; set; } // Propiedad para almacenar el nombre del tipo de usuario (Administrador, Entrenador, Cliente)
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-
         public string Email { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public DateTime FechaIngreso { get; set; }
