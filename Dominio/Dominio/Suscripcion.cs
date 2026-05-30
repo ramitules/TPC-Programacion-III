b8f1c358dd8f9b11840c0536551d5136a715f315
Miguel <fernandezmonzonmiguel@gmail.com>
2026-05-29 22:12:22 -0300

    Dominio (Suscripciones): Se agregaron sus atributos.

--- Dominio/Dominio/Suscripcion.cs
+++ Dominio/Dominio/Suscripcion.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio
 {
-    //Esta clase representa el tipo de suscripción que un usuario puede tener, con un identificador y una descripción de la suscripción.
     public class Suscripcion
     {
-        public int IdSuscripcion{ get; set; } // 1 o 0
-        public string DescripcionSuscripcion{ get; set; }
+        public int IdSuscripcion { get; set; }
+        public Cliente Cliente { get; set; }
+        public Plan Plan { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public bool Activa { get; set; } // Indica si la suscripción está activa o no.
     }
 }
