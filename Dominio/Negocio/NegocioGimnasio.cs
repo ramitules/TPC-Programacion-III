f5deae06b72b32ef1cca2c4aca340bd69cea62cc
ramitules <ramirotules@hotmail.com>
2026-06-04 20:48:20 -0300

    Negocio Cliente parcialmente terminada

--- Dominio/Negocio/NegocioGimnasio.cs
+++ Dominio/Negocio/NegocioGimnasio.cs
@@ -15,53 +15,5 @@ namespace Negocio
             
 
         }
-
-        public List<Usuario> listarTodosLosUsuarios(string sp)
-        {
-            List<Usuario> lista = new List<Usuario>();
-            AccesoADatos datos = new AccesoADatos();
-            try
-            {
-                datos.SetearConsultaSP(sp);
-                datos.ejecutarLectura();
-                while (datos.Lector.Read())
-                {
-                    int rol = Convert.ToInt32(datos.Lector["IdRol"]);
-                    Usuario us;
-                    if (rol == 3)
-                    {
-                        Cliente cl = new Cliente();
-                        cl.PesoCorporal = (decimal)datos.Lector["PesoCorporalKG"];
-                        us = cl;
-                    }
-                    else if (rol == 1)
-                    {
-                        us = new Admin();
-                    }
-                    else if (rol == 2)
-                    {
-                        us = new Profesor();
-                    }
-                    else
-                    {
-                        us = new Administrativo();
-                    }
-                    us.IdUsuario = (int)datos.Lector["IdUsuario"];
-                    us.Nombre = (string)datos.Lector["Nombre"];
-                    us.Apellido = (string)datos.Lector["Apellido"];
-                    us.Email = (string)datos.Lector["Email"];
-                    us.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
-                    us.Rol = Convert.ToInt32(datos.Lector["IdRol"]);
-                    us.FechaIngreso = (DateTime)datos.Lector["FechaIngreso"];
-                    lista.Add(us);
-                }
-                return lista;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error al listar los usuarios: (modulo Negocio listarTodosLosUsuarios)" + e.Message);
-                return lista;
-            }
-        }
     }
 }
