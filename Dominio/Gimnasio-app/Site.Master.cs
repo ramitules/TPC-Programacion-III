cd094f011b57bcad7985200cd8f1fd41dbae248c
ramitules <ramirotules@hotmail.com>
2026-06-26 16:16:38 -0300

    Bug fix rutinas y ejercicios links invertidos + nombre de usuario en su perfil + bug fix iconos de Bootstrap

--- Dominio/Gimnasio-app/Site.Master.cs
+++ Dominio/Gimnasio-app/Site.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Integraciones;
+using Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,13 @@ namespace Gimnasio_app
 {
     public partial class SiteMaster : MasterPage
     {
+        public string NombreUsuario { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (!Seguridad.SessionActiva(Session["usuario"]))
+                NombreUsuario = "Sin sesion";
+            else
+                NombreUsuario = ((Usuario)Session["usuario"]).Nombre;
         }
     }
 }
