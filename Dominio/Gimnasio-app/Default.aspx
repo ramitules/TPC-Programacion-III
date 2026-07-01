6751b58e57b9b89db37576328b78570296225fab
ramitules <ramirotules@hotmail.com>
2026-07-01 19:45:58 -0300

    Ajuste de frontend de Default y Master

--- Dominio/Gimnasio-app/Default.aspx
+++ Dominio/Gimnasio-app/Default.aspx
@@ -4,13 +4,9 @@
 
     <main>
         <section class="row" aria-labelledby="aspnetTitle">
-            <h1 id="aspnetTitle">Panel de control Admin</h1>
-            <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-            <p><a href="http://www.asp.net" class="btn btn-primary btn-md">Learn more &raquo;</a></p>
+            <h1 id="aspnetTitle">Gimnasio web</h1>
+            <p class="lead"><i class="bi bi-arrow-left"></i> Elegí una opción desde la barra lateral</p>
         </section>
-
-        
-
     </main>
 
 </asp:Content>
--- Dominio/Gimnasio-app/Default.aspx.cs
+++ Dominio/Gimnasio-app/Default.aspx.cs
@@ -13,36 +13,7 @@ namespace Gimnasio_app
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // PENDIENTE PARA INICIO DE SESION FORZADA
-
-            //if (Session["rol"] == null)
-            //{
-            //    Response.Redirect("~/Login.aspx", false);
-            //}
-            //else 
-            //{
-            //    if (Session["rol"].ToString() == "1")
-            //    {
-            //        NegocioGimnasio negocio = new NegocioGimnasio();
-            //        List<Usuario> lista = negocio.listarTodosLosUsuarios("sp_ObtenerUsuarios"); ;
-            //        List<Cliente> listaClientes = lista.Where(x => x.IdRol == 3).Cast<Cliente>().ToList();
-            //        List<Profesor> listaProfes = lista.Where(x => x.IdRol == 2).Cast<Profesor>().ToList();
-            //        List<Administrativo> listaAdministrativos = lista.Where(x => x.IdRol == 4).Cast<Administrativo>().ToList();
-            //        List<Admin> listaAdmin = lista.Where(x => x.IdRol == 1).Cast<Admin>().ToList();
-
-            //        dgvCliente.DataSource = listaClientes;
-            //        dgvCliente.DataBind();
-
-            //        dgvProfesor.DataSource = listaProfes;
-            //        dgvProfesor.DataBind();
-
-            //        dgvAdministrador.DataSource = listaAdministrativos;
-            //        dgvAdministrador.DataBind();
-
-            //        dgvAdmin.DataSource = listaAdmin;
-            //        dgvAdmin.DataBind();
-            //    } 
-            //}
+            
         }
     }
 }
